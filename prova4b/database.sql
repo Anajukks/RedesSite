7c589a24170fe3f096919f025e3a15bfacd69a04
Ana Júlia <111990344+Anajukks@users.noreply.github.com>
2023-10-06 08:59:47 -0300

    Update database.sql

--- prova4b/database.sql
+++ prova4b/database.sql
@@ -7,8 +7,3 @@ CREATE TABLE usuarios (
     username VARCHAR(255) NOT NULL,
     senha VARCHAR(255) NOT NULL
 );
-
-INSERT INTO usuarios (username, senha) VALUES
-('usuario1', 'senha1'),
-('usuario2', 'senha2'),
-('usuario3', 'senha3');
